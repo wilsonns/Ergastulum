6830666c26613528b98e0ca51a3136fb351896cf
wilsonns <30836552+wilsonns@users.noreply.github.com>
2021-05-26 10:39:39 -0300

    Update Corpo.h

--- include/Corpo.h
+++ include/Corpo.h
@@ -1,1 +1,16 @@
+#ifndef CORPO_H
+#define CORPO_H
+
+#include<main.h>
+
+class Parte
+{
+  public:
+    Parte* pai;
+    std::vector<Parte*> filhos;
+    int hp;
+    int maxHp;
+};
+
+#endif // CORPO_H
 
