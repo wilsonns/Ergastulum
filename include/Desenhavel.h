e0875d56b41ed20fc437d98583f4b3ece7b720dd
wilsonns <30836552+wilsonns@users.noreply.github.com>
2021-10-27 07:57:04 -0300

    Update Desenhavel.h

--- include/Desenhavel.h
+++ include/Desenhavel.h
@@ -1,10 +1,13 @@
+class Entidade;
+
 class Desenhavel
 {
  private:
   char _simbolo;
-  
+  Entidade* self = nullptr;
  public:
-  Desenhavel(char simbolo);
+  Desenhavel(char simbolo, Entidade* self);
+  ~Desenhavel();
   void render(int x, int y);
   char simbolo();
 
